bf6af78b54810ff5da396673b78de60b572bc352
Piorosen <aoikazto@gmail.com>
2020-06-07 16:38:36 +0900

    create init

---
+++ Audio2Tag/Audio2Tag/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Audio2Tag
+//
+//  Created by Aoikazto on 2020/06/04.
+//  Copyright © 2020 Aoikazto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
