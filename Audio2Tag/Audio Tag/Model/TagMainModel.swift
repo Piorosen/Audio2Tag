ffefff6142bdfc4332ff52d735ff762df2d5f807
Piorosen <aoikazto@gmail.com>
2020-06-22 13:11:44 +0900

    maked tagMainPage

---
+++ Audio2Tag/Audio Tag/Model/TagMainModel.swift
@@ -0,0 +1,9 @@
+//
+//  TagMainModel.swift
+//  Audio2Tag
+//
+//  Created by Aoikazto on 2020/06/22.
+//  Copyright © 2020 Aoikazto. All rights reserved.
+//
+
+import Foundation
---
+++ Audio2Tag/Audio Tag/View/TagMainView.swift
@@ -0,0 +1,21 @@
+//
+//  TagMainView.swift
+//  Audio2Tag
+//
+//  Created by Aoikazto on 2020/06/22.
+//  Copyright © 2020 Aoikazto. All rights reserved.
+//
+
+import SwiftUI
+
+struct TagMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TagMainView_Previews: PreviewProvider {
+    static var previews: some View {
+        TagMainView()
+    }
+}
