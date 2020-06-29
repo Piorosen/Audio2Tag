9dfee8a35b52be2f427f5b952602e3179bf186db
Piorosen <aoikazto@gmail.com>
2020-06-29 15:46:38 +0900

    resource change

--- Audio2TagTests/CueSplitTest.swift
+++ Audio2TagTests/CueSplitTest.swift
@@ -13,49 +13,57 @@ import CueSheet
 @testable import Audio2Tag
 
 class Audio2TagTests: XCTestCase {
-    private let resources = Bundle(for: type(of: self)).bundleURL.appendingPathComponent("Contents").appendingPathComponent("Resources")
+    func getResources() -> URL {
+        return Bundle(for: type(of: self)).bundleURL.appendingPathComponent("Contents").appendingPathComponent("Resources")
+    }
     
     override func setUp() {
         
     }
-
+    
     override func tearDown() {
-       
+        
     }
     
     func testCueSplit() {
         
-        
     }
     
-
     func testCueRead() {
-        if let list = try? FileManager.default.contentsOfDirectory(at: resources, includingPropertiesForKeys: nil) {
-            for i in list {
-                let fileDir = resources.appendingPathComponent(i.lastPathComponent, isDirectory: true)
-                    
-                if let list = try? FileManager.default.contentsOfDirectory(at: fileDir, includingPropertiesForKeys: nil) {
-                    guard let name = list.first(where: { url in url.pathExtension.lowercased() == "cue" }) else {
-                        XCTFail("not found : cue file")
-                        return
-                    }
-                    
-                    let parser = CueSheetParser()
-                    guard let cue = parser.Load(path: name) else {
-                        XCTFail("load fail : cue file")
-                        return
-                    }
-                    let music = name.deletingLastPathComponent().appendingPathComponent(cue.file.fileName)
-                    
-                    guard parser.loadFile(pathOfMusic: music, pathOfCue: name) != nil else {
-                        XCTFail("load fail : cue file and Music file")
-                        return
-                    }
-                }
+        let resources = getResources()
+        
+        guard let list = try? FileManager.default.contentsOfDirectory(at: resources, includingPropertiesForKeys: nil) else {
+            XCTFail("not found : resource folder")
+            return
+        }
+        
+        for i in list {
+            let fileDir = resources.appendingPathComponent(i.lastPathComponent, isDirectory: true)
+            
+            guard let list = try? FileManager.default.contentsOfDirectory(at: fileDir, includingPropertiesForKeys: nil) else {
+                XCTFail("not found : test resource folder list")
+                return
+            }
+            
+            guard let name = list.first(where: { url in url.pathExtension.lowercased() == "cue" }) else {
+                XCTFail("not found : cue file")
+                return
+            }
+            
+            let parser = CueSheetParser()
+            guard let cue = parser.Load(path: name) else {
+                XCTFail("load fail : cue file")
+                return
+            }
+            let music = name.deletingLastPathComponent().appendingPathComponent(cue.file.fileName)
+            
+            guard parser.loadFile(pathOfMusic: music, pathOfCue: name)?.info != nil else {
+                XCTFail("load fail : cue file and Music file")
+                return
             }
         }
     }
-
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
