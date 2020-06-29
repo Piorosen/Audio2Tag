7537c5f9e83f7971c8d787fdedfbc9a6d895500f
Piorosen <aoikazto@gmail.com>
2020-06-29 09:54:51 +0900

    append unit test file

--- Audio2TagTests/Audio2TagTests.swift
+++ Audio2TagTests/Audio2TagTests.swift
@@ -11,17 +11,17 @@ import XCTest
 
 class Audio2TagTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        print("hello nice to meet you.sssss")
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+       
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        
     }
 
     func testPerformanceExample() throws {
