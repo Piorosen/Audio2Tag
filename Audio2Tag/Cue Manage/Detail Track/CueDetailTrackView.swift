7baea49cd0c15d57ad3db036ff74941860feae04
Piorosen <aoikazto@gmail.com>
2020-07-21 13:05:16 +0900

    NavigationView 중첩하면 오류 생김

--- Audio2Tag/Cue Manage/Detail Track/CueDetailTrackView.swift
+++ Audio2Tag/Cue Manage/Detail Track/CueDetailTrackView.swift
@@ -17,75 +17,73 @@ struct CueDetailTrackView: View {
     }
     
     var body: some View {
-        NavigationView {
-            List {
-                Section(header: Text("Description")) {
-                    HStack {
-                        Text("Title")
-                        Spacer()
-                        Text("\(self.viewModel.item.track.title)")
-                    }
-                    HStack {
-                        Text("Track Num")
-                        Spacer()
-                        Text("\(self.viewModel.item.track.trackNum)")
-                    }
-                    HStack {
-                        Text("ISRC")
-                        Spacer()
-                        Text("\(self.viewModel.item.track.isrc)")
-                    }
-                    HStack {
-                        Text("Performer")
-                        Spacer()
-                        Text("\(self.viewModel.item.track.performer)")
-                    }
-                    
-                    HStack {
-                        Text("Track Type")
-                        Spacer()
-                        Text("\(self.viewModel.item.track.trackType)")
-                    }
-                    HStack {
-                        Text("Song Writer")
-                        Spacer()
-                        Text("\(self.viewModel.item.track.songWriter)")
-                    }
+        List {
+            Section(header: Text("Description")) {
+                HStack {
+                    Text("Title")
+                    Spacer()
+                    Text("\(self.viewModel.item.track.title)")
                 }
-                Section(header: Text("Rem")) {
-                    ForEach (self.viewModel.rem) { item in
-                        HStack {
-                            Text("\(item.value.key)")
-                            Spacer()
-                            Text("\(item.value.value)")
-                        }
-                    }
+                HStack {
+                    Text("Track Num")
+                    Spacer()
+                    Text("\(self.viewModel.item.track.trackNum)")
+                }
+                HStack {
+                    Text("ISRC")
+                    Spacer()
+                    Text("\(self.viewModel.item.track.isrc)")
+                }
+                HStack {
+                    Text("Performer")
+                    Spacer()
+                    Text("\(self.viewModel.item.track.performer)")
                 }
                 
-                Section(header: Text("Time Info")) {
+                HStack {
+                    Text("Track Type")
+                    Spacer()
+                    Text("\(self.viewModel.item.track.trackType)")
+                }
+                HStack {
+                    Text("Song Writer")
+                    Spacer()
+                    Text("\(self.viewModel.item.track.songWriter)")
+                }
+            }
+            Section(header: Text("Rem")) {
+                ForEach (self.viewModel.rem) { item in
                     HStack {
-                        Text("Start Time")
+                        Text("\(item.value.key)")
                         Spacer()
-                        Text(self.viewModel.startTime)
-                    }
-                    HStack {
-                        Text("End Time")
-                        Spacer()
-                        Text(self.viewModel.endTime)
-                    }
-                    HStack {
-                        Text("Wait Time")
-                        Spacer()
-                        Text(self.viewModel.waitTime)
-                    }
-                    HStack {
-                        Text("Duration Time")
-                        Spacer()
-                        Text(self.viewModel.durTime)
+                        Text("\(item.value.value)")
                     }
                 }
-                
-            }.navigationBarTitle("Track Info")
-        }
+            }
+            
+            Section(header: Text("Time Info")) {
+                HStack {
+                    Text("Start Time")
+                    Spacer()
+                    Text(self.viewModel.startTime)
+                }
+                HStack {
+                    Text("End Time")
+                    Spacer()
+                    Text(self.viewModel.endTime)
+                }
+                HStack {
+                    Text("Wait Time")
+                    Spacer()
+                    Text(self.viewModel.waitTime)
+                }
+                HStack {
+                    Text("Duration Time")
+                    Spacer()
+                    Text(self.viewModel.durTime)
+                }
+            }
+            
+        }.navigationBarTitle("Track Info")
     }
 }
