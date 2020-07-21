2c357cab1464b36c5a53fcca4623f6847e2a06ce
Piorosen <aoikazto@gmail.com>
2020-07-22 00:41:35 +0900

    돌아가는거 보존해야함

--- Audio2Tag/Cue Manage/Main/AVAudioFileConverter.swift
+++ Audio2Tag/Cue Manage/Main/AVAudioFileConverter.swift
@@ -26,7 +26,7 @@ public final class AVAudioFileConverter {
         asset = AVAsset(url: inputURL)
         
         guard let file = try? AVAudioFile(forReading: inputURL) else { return nil }
-        guard let tracks = asset.tracks(withMediaType: AVMediaType.audio).first else { return nil }
+        guard let tracks = asset.tracks(withMediaType: .audio).first else { return nil }
         
         track = tracks
         outputSettings = file.fileFormat.settings
@@ -34,64 +34,89 @@ public final class AVAudioFileConverter {
     
     public func convert(callback: @escaping (_ index:Int, _ ownProgress:Float, _ totalProgress:Float) -> Void) {
         asset.loadValuesAsynchronously(forKeys: ["tracks"]) {
-            var progress = [Float](repeating: 0, count: self.outputURL.count)
+            var sum = [Float](repeating: 0, count: self.outputURL.count)
+            
             for i in self.outputURL.indices {
-                var localError:NSError?
-                var success = self.asset.statusOfValue(forKey: "tracks", error: &localError) == AVKeyValueStatus.loaded
-                assert(success, "Ha..")
-                
-                
-                let result = self.startAssetReaderAndWriter(value: self.outputURL[i]) { [i] p in
-                    progress[i] = Float(1) / Float(self.outputURL.count) * p
-                    callback(i, p, progress.reduce(0, +))
+                let _ = self.startAssetReaderAndWriter(value: self.outputURL[i]) { [i] p in
+                    let _ = Float(i) / Float(self.outputURL.count)
+                    let size = Float(1) / Float(self.outputURL.count) * p
+                    sum[i] = size
+                    callback(i, size, sum.reduce(0, +))
                 }
-                print("\(result)")
+                
             }
         }
     }
     
     func startAssetReaderAndWriter(value: (url:URL, range:CMTimeRange), percentCallback: @escaping (Float) -> Void) -> Bool {
-        guard let assetWriter = try? AVAssetWriter(outputURL: value.url, fileType: .wav) else {
+        let assetReader = try? AVAssetReader(asset: asset)
+        let assetWriter = try? AVAssetWriter(outputURL: value.url, fileType: .wav)
+        
+        var assetAudioTrack:AVAssetTrack? = nil
+        let audioTracks = asset.tracks(withMediaType: .audio)
+        
+        if (audioTracks.count > 0) {
+            assetAudioTrack = audioTracks[0]
+        }
+        
+        guard let file = try? AVAudioFile(forReading: inputURL) else {
+            return false
+        }
+        let outputSettings = file.fileFormat.settings
+        
+        if (assetAudioTrack == nil) {
             return false
         }
         
-        guard let assetReader = try? AVAssetReader(asset: asset) else {
-            return false
-        }
+        let assetReaderAudioOutput = AVAssetReaderTrackOutput(track: assetAudioTrack!, outputSettings: [AVFormatIDKey:Int(kAudioFormatLinearPCM)])
         
-        let assetReaderAudioOutput = AVAssetReaderTrackOutput(track: track, outputSettings: [AVFormatIDKey:Int(kAudioFormatLinearPCM)])
-        assetReader.add(assetReaderAudioOutput)
+        assetReader!.add(assetReaderAudioOutput)
         
         let assetWriterAudioInput = AVAssetWriterInput(mediaType: AVMediaType.audio, outputSettings: outputSettings)
-        assetWriter.add(assetWriterAudioInput)
-        assetWriter.startWriting()
-        assetWriter.startSession(atSourceTime: CMTime.zero)
         
-        assetWriterAudioInput.requestMediaDataWhenReady(on: dispatch) {
-            assetReader.timeRange = value.range
-            assetReader.startReading()
-            
-            while(assetWriterAudioInput.isReadyForMoreMediaData) {
-                if let sampleBuffer = assetReaderAudioOutput.copyNextSampleBuffer() {
-                    let timeStamp = CMSampleBufferGetPresentationTimeStamp(sampleBuffer)
+        assetWriter!.add(assetWriterAudioInput)
+        
+        
+        // MARK: - ga
+        let delay = DispatchSemaphore(value: 0)
+        print("STARTING ASSET WRITER")
+        
+        assetReader!.timeRange = value.range
+        
+        assetWriter!.startWriting()
+        assetReader!.startReading()
+        assetWriter!.startSession(atSourceTime: CMTime.zero)
+        //    assetWriter.endSession(atSourceTime: CMTime(seconds: 1000, preferredTimescale: 1))
+        //    assetWriter.endSession(atSourceTime: CMTime(seconds: 100, preferredTimescale: 1))
+        
+        assetWriterAudioInput.requestMediaDataWhenReady(on: DispatchQueue(label: "gogogo")) {
+            while(assetWriterAudioInput.isReadyForMoreMediaData ) {
+                var sampleBuffer = assetReaderAudioOutput.copyNextSampleBuffer()
+                
+                if(sampleBuffer != nil) {
+                    let timeStamp = CMSampleBufferGetPresentationTimeStamp(sampleBuffer!)
                     let timeSecond = CMTimeGetSeconds(timeStamp - value.range.start)
                     
                     let per = timeSecond / CMTimeGetSeconds(value.range.duration)
                     percentCallback(Float(per))
                     
-                    assetWriterAudioInput.append(sampleBuffer)
-                    continue
-                }else {
+                    assetWriterAudioInput.append(sampleBuffer!)
+                    sampleBuffer = nil
+                } else {
                     assetWriterAudioInput.markAsFinished()
-                    assetReader.cancelReading()
-                    assetWriter.finishWriting {
-                        percentCallback(1)
+                    assetReader!.cancelReading()
+                    assetWriter!.finishWriting {
+                        delay.signal()
+                        print("Asset Writer Finished Writing")
                     }
                     break
                 }
             }
+            
+            percentCallback(1)
         }
         
+        delay.wait()
         return true
     }
 }
