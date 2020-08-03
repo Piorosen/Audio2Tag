665ab3ca9c65705018a6a091a9d48f546b111790
Piorosen <aoikazto@gmail.com>
2020-08-03 22:18:08 +0900

    feat : SplitMusicStatusView 만듬

--- Audio2Tag/Cue Manage/Split Music/SplitMusicView.swift
+++ Audio2Tag/Cue Manage/Split Music/SplitMusicView.swift
@@ -8,21 +8,48 @@
 
 import SwiftUI
 
-
+extension Color {
+    static func randColor() -> Color {
+        return Color(red: Double.random(in: 0...1), green: Double.random(in: 0...1), blue: Double.random(in: 0...1))
+    }
+}
 
 struct SplitMusicView: View {
-    @Binding var bind: [SplitMusicModel]
+    //    @Binding var bind: [SplitMusicModel]
+    @Binding var isPresented: Bool
+    let data = [1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9]
+    // 민지,
+    //
+    
     
     var body: some View {
         Group {
-            List(bind, id: \.self.name) { value in
-                VStack {
-                    Text("\(value.name) \(value.status)")
-                    
+            VStack {
+                ScrollView {
+                    ForEach(self.data, id: \.self) { value in
+                        Text("\(value)").frame(height: 40)
+                            .frame(maxWidth: .infinity)
+                            .background(Color.randColor())
+                            
+                    }.frame(maxWidth: .infinity, maxHeight: .infinity)
                 }
-            }.frame(maxWidth: .infinity, maxHeight: .infinity)
+                
+                Button(action: {
+                    self.isPresented = false
+                }) {
+                    Text("OK")
+                }
+                .padding(10).frame(maxWidth: .infinity)
+                
+            }
+            .background(Color(UIColor.systemBackground))
+            .cornerRadius(15)
+            .shadow(radius: 100)
+            .overlay(RoundedRectangle(cornerRadius: 15).stroke(Color.black, lineWidth: 1))
         }
-        
+        .frame(width: 300, height: 400)
+        .offset(y: self.isPresented ? 0 : UIScreen.main.bounds.height)
+        .animation(.spring())
     }
 }
 
