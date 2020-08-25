930ac0d65e85964febd4c9e48418f59f573d6985
Piorosen <aoikazto@gmail.com>
2020-08-25 19:42:29 +0900

    feat : tdd 기반 개발 시작

--- Audio2TagTests/Tag Manage/Audio2TagSearchTests.swift
+++ Audio2TagTests/Tag Manage/Audio2TagSearchTests.swift
@@ -1,28 +1,26 @@
 //
-//  Audio2TagTests.swift
+//  Audio2TagSearchTests.swift
 //  Audio2TagTests
 //
-//  Created by Aoikazto on 2020/08/11.
+//  Created by Aoikazto on 2020/08/25.
 //  Copyright © 2020 Aoikazto. All rights reserved.
 //
 
 import XCTest
-@testable import Audio2Tag
 
-class Audio2TagTests: XCTestCase {
+class Audio2TagSearchTests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called be1fore the invocation of each test method in the class.
+        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    // VgmDB 검색
+    
+    // 
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
