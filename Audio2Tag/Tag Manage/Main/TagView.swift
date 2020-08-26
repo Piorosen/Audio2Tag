b65150d37700a2f8aa1c8126bdf022cda0e600a3
Piorosen <aoikazto@gmail.com>
2020-08-26 18:40:17 +0900

    feat : 분할함.

--- Audio2Tag/Tag Manage/Main/TagView.swift
+++ Audio2Tag/Tag Manage/Main/TagView.swift
@@ -10,38 +10,29 @@ import SwiftUI
 
 struct TagView: View {
     @ObservedObject var viewModel = TagViewModel()
-    @State var backgroundColor = Color.yellow
     
     var body: some View {
         NavigationView{
-            // Image
             VStack {
-                Spacer()
-                
-            }.background(backgroundColor)
+   
+            }
             .navigationTitle("Tag Info")
-            .navigationBarItems(trailing: Group {
-                HStack {
-                    Button(action: viewModel.traillingButtonAction) {
-                        Image(systemName: "plus.app")
-                    }
-                    Button(action: { viewModel.openActionSheet = true }) {
-                        Image(systemName: "doc.on.doc")
-                    }.sheet(isPresented: $viewModel.openSheet) {
-                        TagSearchView().setKind(kind: .VgmDB)
+            .navigationBarItems(trailing: TagNavigationTraillingView().onTagReuqest {
+                viewModel.openSheet = true
+            })
+        }
+        .sheet(isPresented: $viewModel.openSheet) {
+            TagSearchView().setKind(kind: .VgmDB)
 //                        DocumentPicker()
 //                            .setConfig(folderPicker: false, allowMultiple: false)
 //                            .setUTType(type: [.folder])
 //                            .onSelectFile { viewModel.loadAudio(url: $0) }
-                    }.actionSheet(isPresented: $viewModel.openActionSheet) {
-                        ActionSheet(title: Text("검색"), message: Text("Tag 정보를 검색합니다."), buttons: [
-                            .default(Text("VgmDB")) { self.backgroundColor = .red },
-                            .default(Text("MusicBrainz")) { self.backgroundColor = .green },
-                            .cancel()
-                        ])
-                    }
-                }
-            })
+        }.actionSheet(isPresented: $viewModel.openActionSheet) {
+            ActionSheet(title: Text("검색"), message: Text("Tag 정보를 검색합니다."), buttons: [
+                .default(Text("VgmDB")) { },
+                .default(Text("MusicBrainz")) { },
+                .cancel()
+            ])
         }
     }
 }
