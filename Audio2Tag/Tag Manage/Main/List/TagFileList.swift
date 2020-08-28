e146b3d26fd7ee681f68939363a58bb5457d488a
Piorosen <aoikazto@gmail.com>
2020-08-28 12:04:10 +0900

    feat : 파일 리스트

---
+++ Audio2Tag/Tag Manage/Main/List/TagFileList.swift
@@ -0,0 +1,21 @@
+//
+//  TagFileList.swift
+//  Audio2Tag
+//
+//  Created by Aoikazto on 2020/08/28.
+//  Copyright © 2020 Aoikazto. All rights reserved.
+//
+
+import SwiftUI
+
+struct TagFileList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TagFileList_Previews: PreviewProvider {
+    static var previews: some View {
+        TagFileList()
+    }
+}
