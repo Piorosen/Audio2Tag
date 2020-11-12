1df368c121b813c4e02a6288287c1bbfd04a2767
Piorosen <aoikazto@gmail.com>
2020-11-12 13:46:05 +0900

    move : 파일 이동

--- Audio2Tag/ContentView.swift
+++ Audio2Tag/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
     
     var body: some View {
         TabView {
-            CueView.tabItem {
+            CueView().tabItem {
                 Text("Cue")
             }
 //
---
+++ Audio2Tag/Cue/View/CueView.swift
@@ -0,0 +1,20 @@
+//
+//  CueView.swift
+//  Audio2Tag
+//
+//  Created by Aoikazto on 2020/11/12.
+//
+
+import SwiftUI
+
+struct CueView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CueView_Previews: PreviewProvider {
+    static var previews: some View {
+        CueView()
+    }
+}
